f485f7e0a71f67e8d057aea0d4b953a0ae11a62a
wala <chaar.wala@gmail.com>
2024-05-11 15:09:49 +0100

    fdljjbgfo

---
+++ src/main/webapp/app/services/demande-en-cours/notification.model.ts
@@ -0,0 +1,12 @@
+export interface INotification {
+  initiateur: string;
+  type: string;
+  collaborateur: string;
+  taskName: string;
+  processInstanceId: string;
+  demande: string;
+  ss;
+  dueDate: Date;
+  createdAt: Date;
+  taskId: string;
+}
